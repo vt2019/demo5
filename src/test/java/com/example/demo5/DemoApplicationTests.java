e54679210adbb50f3e391b00b165cb6771dba05d
JX User <jxuser@pingworks.de>
2018-12-18 09:16:44 +0100

    creating failing test

--- src/test/java/com/example/demo5/DemoApplicationTests.java
+++ src/test/java/com/example/demo5/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo5;
 
 import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +12,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertFalse( true );
 	}
 
 }
